89ed3b809b55ca4ebaa708f2b54b51ef740fc6ad
Asterak <prasek.matej@seznam.cz>
2018-04-17 22:45:27 +0200

    Created adder block

---
+++ CircuitSimulator-BL/src/Blocks/AdderBlock.java
@@ -0,0 +1,28 @@
+package Blocks;
+
+import Data.NumericData;
+import Ports.InputNumericPort;
+import Ports.OutputNumericPort;
+
+public class AdderBlock extends BlockBase {
+
+    private InputNumericPort input1;
+    private InputNumericPort input2;
+    private OutputNumericPort output;
+
+    public AdderBlock() {
+        this.input1 = new InputNumericPort();
+        this.input2 = new InputNumericPort();
+        this.output = new OutputNumericPort();
+        this.inputPorts.add(input1);
+        this.inputPorts.add(input2);
+        this.outputPorts.add(output);
+    }
+
+    @Override
+    public void Execute() {
+        double result = input1.DownloadData().Data + input2.DownloadData().Data;
+        output.Send(new NumericData(result));
+    }
+
+}
