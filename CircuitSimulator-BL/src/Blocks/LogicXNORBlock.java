55a005535dd58def536199fecae681105e3b5d6e
Asterak <prasek.matej@seznam.cz>
2018-04-18 17:43:09 +0200

    Implemented LogicXNORBlock

---
+++ CircuitSimulator-BL/src/Blocks/LogicXNORBlock.java
@@ -0,0 +1,28 @@
+package Blocks;
+
+import Data.BooleanData;
+import Ports.InputBooleanPort;
+import Ports.OutputBooleanPort;
+
+public class LogicXNORBlock extends BlockBase {
+
+    private InputBooleanPort input1;
+    private InputBooleanPort input2;
+    private OutputBooleanPort output;
+
+    public LogicXNORBlock() {
+        this.input1 = new InputBooleanPort();
+        this.input2 = new InputBooleanPort();
+        this.output = new OutputBooleanPort();
+        this.inputPorts.add(input1);
+        this.inputPorts.add(input2);
+        this.outputPorts.add(output);
+    }
+
+    @Override
+    public void Execute() {
+        boolean result = input1.DownloadData().Data == input2.DownloadData().Data;
+        output.Send(new BooleanData(result));
+    }
+
+}
