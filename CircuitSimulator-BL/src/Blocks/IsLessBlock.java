d331c5a39db447c6df5cdd80258cc093d488f4a8
Asterak <prasek.matej@seznam.cz>
2018-04-18 17:41:15 +0200

    Implemented IsLessBlock

---
+++ CircuitSimulator-BL/src/Blocks/IsLessBlock.java
@@ -0,0 +1,28 @@
+package Blocks;
+
+import Data.BooleanData;
+import Ports.InputNumericPort;
+import Ports.OutputBooleanPort;
+
+public class IsLessBlock extends BlockBase {
+
+    private InputNumericPort input1;
+    private InputNumericPort input2;
+    private OutputBooleanPort output;
+
+    public IsLessBlock() {
+        this.input1 = new InputNumericPort();
+        this.input2 = new InputNumericPort();
+        this.output = new OutputBooleanPort();
+        this.inputPorts.add(input1);
+        this.inputPorts.add(input2);
+        this.outputPorts.add(output);
+    }
+
+    @Override
+    public void Execute() {
+        boolean result = input1.DownloadData().Data < input2.DownloadData().Data;
+        output.Send(new BooleanData(result));
+    }
+
+}
